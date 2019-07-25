5b372525452715a5dc6934976858745c5df4fa38
jennifer <jennifer.malleret@outlook.fr>
2019-07-25 11:35:31 +0200

    exercice 2 php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 2</title>
+</head>
+<body>
+  <h1>Variable answer</h1>
+
+    <?php
+      $answer = 'no';
+      if($answer == 'yes'){?>
+        <p><?='Vous avez cliquez yes';?></p>
+    <?php  }
+      else {?>
+        <p><?='Vous avez cliquez no';?></p>
+      <?php } ?>
+    </body>
+    </html>
